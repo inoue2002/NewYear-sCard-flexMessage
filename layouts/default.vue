797a44137072d50aca517a849c1f4144939d84a0
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2020-12-31 21:28:31 +0900

    色の調整

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app dark>
-    <v-app-bar :clipped-left="clipped" fixed app>
+    <v-app-bar fixed app color="#00db45">
       <v-toolbar-title v-text="title" />
     </v-app-bar>
     <v-main>
@@ -8,20 +8,8 @@
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+      <span>&copy; 2020/2021 ようかん</span>
     </v-footer>
   </v-app>
 </template>
